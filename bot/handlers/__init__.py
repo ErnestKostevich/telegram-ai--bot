35d40b938f08da76328da3bc539f7cf0526735b2
Ernest Kostevich <ernest2011kostevich@gmail.com>
2026-05-10 22:34:52 +0200

    Fix missing handler imports in __init__.py causing AttributeError

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,12 +1,12 @@
-from .base import start_command, help_command, info_command, status_command
-from .ai_memory import setprovider_command, setkey_command, ai_command, memorysave_command, memoryget_command, memorylist_command, memorydel_command
-from .notes import note_command, notes_command, delnote_command
+from .base import start_command, help_command, info_command, status_command, lang_command, profile_command, disco_command
+from .ai_memory import setprovider_command, setkey_command, setmodel_command, ai_command, memorysave_command, memoryget_command, memorylist_command, memorydel_command
+from .notes import note_command, notes_command, delnote_command, todo_command
 from .vip_creator import vip_command, remind_command, reminders_command, grant_vip_command, broadcast_command, stats_command
-from .groups import grouphelp_command, ban_command, ask_command, rules_command, setrules_command
+from .groups import grouphelp_command, ban_command, ask_command, rules_command, setrules_command, guardian_command
 
 from .interactive import button_callback, keyboard_message_handler
 from .games import dice_command, coinflip_command, random_command, joke_command
-from .media import media_message_handler
+from .media import media_message_handler, generate_command
 from .utils import time_command, weather_command, calc_command, password_command
 
 __all__ = [
